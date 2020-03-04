922caf8e5444589f8c2ce70e261f71d65188ab8e
basharovi <hello@basharovi.tech>
2020-03-04 22:04:23 +0600

    Learned about selector and Template

--- MyAngularApp/src/app/app.component.ts
+++ MyAngularApp/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: '<h1> Heading </h1>' +
+    '<hr><h2>Template</h2>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'My First Angular App';
+  just = 'Nothing!'
 }
