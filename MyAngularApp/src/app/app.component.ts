904d7ec02e87788d42fd9d3cd84552ac67e489bd
basharovi <hello@basharovi.tech>
2020-03-04 22:17:54 +0600

    Learned about preserveWhitespaces

--- MyAngularApp/src/app/app.component.ts
+++ MyAngularApp/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: '<h1> Heading </h1>' +
-    '<hr><h2>Template</h2>',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  // template: '<h1> Heading </h1>' +
+  //   '<hr><h2>Template</h2>',
+  styleUrls: ['./app.component.css'],
+  preserveWhitespaces:true
 })
 export class AppComponent {
   title = 'My First Angular App';
